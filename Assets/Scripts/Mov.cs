7e6da9697544a41be2b2ddb54f400e589fca12d6
David García <davidgarciasanchez09@gmail.com>
2023-09-09 01:36:01 -0500

    Script Player Controller

--- Assets/Scripts/Mov.cs
+++ Assets/Scripts/Mov.cs
@@ -4,40 +4,39 @@ using UnityEngine;
 
 public class Mov : MonoBehaviour
 {
-    private CharacterController  _controller;
-    [SerializeField] private float _gravity = 0.3f;
-    [SerializeField] private float _speed = 5;
-    [SerializeField] private float _jumpHeight = 15.0f;
-    private float _yVelocity;
-    private bool _canDoubleJump;
+  private CharacterController  _controller;
+  [SerializeField] private float _gravity = 0.3f;
+  [SerializeField] private float _speed = 5;
+  [SerializeField] private float _jumpHeight = 15.0f;
+  private float _yVelocity;
+  private bool _canDoubleJump;
 
-    void Start()=>_controller = GetComponent<CharacterController>();
+  void Start()=>_controller = GetComponent<CharacterController>();
 
-    // Update is called once per frame
-    void Update(){
-     float horizontalInput = Input.GetAxis("Horizontal");
-     Vector3 direction = new Vector3(horizontalInput, 0, 0);
-     Vector3 velocity = direction * _speed;
+  void Update(){
+    float horizontalInput = Input.GetAxis("Horizontal");
+    Vector3 direction = new Vector3(horizontalInput, 0, 0);
+    Vector3 velocity = direction * _speed;
 
-     if (_controller.isGrounded){
-         _canDoubleJump = false;  
+    if (_controller.isGrounded){
+      _canDoubleJump = false;  
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-         _yVelocity = _jumpHeight;
-         _canDoubleJump = true;  
-        }
-     }
-     else{
-       if (Input.GetKeyDown(KeyCode.Space) && _canDoubleJump){
-        _yVelocity += _jumpHeight;
-        _canDoubleJump = false;
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        _yVelocity = _jumpHeight;
+        _canDoubleJump = true;  
+      }
+    }
+    else{
+      if (Input.GetKeyDown(KeyCode.Space) && _canDoubleJump){
+      _yVelocity += _jumpHeight;
+      _canDoubleJump = false;
        
-       }
-         _yVelocity -= _gravity;
-     }
+      }
+      _yVelocity -= _gravity;
+    }
     velocity.y = _yVelocity;
 
     _controller.Move(velocity * Time.deltaTime);
-    }
+  }
 }
